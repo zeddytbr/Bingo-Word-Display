7b0ed947d4fdc8766b2a70abc273b10a9362bc16
Tom Rolfe <55812479+zeddytbr@users.noreply.github.com>
2020-06-01 17:13:34 +0100

    Create bingo words.py

---
+++ bingo words.py
@@ -0,0 +1,12 @@
+while True:
+    words = ["Kelly’s Eye","One Little Duck","Cup of Tea","Knock at the Door","Man Alive","Tom Mix","Lucky Seven","Garden Gate","Doctor’s Orders","Cameron’s Den","Legs 11","One Dozen","Unlucky for Some","Valentine’s Day","Young and Keen","Sweet 16","Dancing Queen","Coming of Age","Goodbye Teens","One Score","Royal Salute","Two Little Ducks","Thee and Me","Two Dozen","Duck and Dive","Pick and Mix","Gateway to Heaven","Over Weight","Rise and Shine","Dirty Gertie","Get Up and Run","Buckle My Shoe","Dirty Knee","Ask for More","Jump and Jive","Three Dozen","More than 11","Christmas Cake","Steps","Naughty 40","Time for Fun","Winnie the Pooh","Down on Your Knees","Droopy Drawers","Halfway There","Up to Tricks","Four and Seven","Four Dozen","PC","Half a Century","Tweak of the Thumb","Danny La Rue","Stuck in the Tree","Clean the Floor","Snakes Alive","Was She Worth It?","Heinz Varieties","Make Them Wait","Brighton Line","Five Dozen","Bakers Bun","Turn the Screw","Tickle Me 63","Red Raw","Old Age Pension","Clickety Click","Made in Heaven","Saving Grace","Either Way Up","Three Score and 10","Bang on the Drum","Six Dozen","Queen B","Candy Store","Strive and Strive","Trombones","Sunset Strip","Heaven’s Gate","One More Time","Eight and Blank","Stop and Run","Straight On Through","Time for Tea","Seven Dozen","Staying Alive","Between the Sticks","Torquay in Devon","Two Fat Ladies","Nearly There","Top of the Shop",]
+    number = input()
+    if number > "90":
+        pass
+    elif number >= "100":
+        pass
+    elif number < "1":
+        pass
+    else:
+        option = int(number)-1
+        print(words[option] + "\n\n")
